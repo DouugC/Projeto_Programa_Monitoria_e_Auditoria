3f922c85a6aaa67966543d26e6396a075254595f
2105066719 <douglassk15@hotmail.com>
2023-03-07 18:04:50 -0300

    Atualização

--- TKINTER 1.py
+++ TKINTER 1.py
@@ -1,4 +1,3 @@
-import tkinter
 from tkinter import *
 from datetime import date
 import numpy as np
@@ -13,11 +12,12 @@ All_rights_reserved = '  ©  Todos os direitos reservados, Douglas Costa'
 
 # valores dos itens do checklist
 
-item1_value = [0.05, '5%']
-item2_value = [0.10, '10%']
-item3_value = [0.20, '20%']
-item4_value = [0.50, '20%']
-item5_value = [0.15, '15%']
+item_value10 = [0.10, '10%']
+item_value15 = [0.15, '15%']
+item_value20 = [0.20, '20%']
+item_value25 = [0.25, '25%']
+item_value30 = [0.30, '30%']
+item_value100 = [1.00, '100%']
 
 
 # Função para gerar os números Random -----------------------------------------------------------
@@ -35,7 +35,7 @@ def preencher_obrigatorio():
         messagebox.showinfo(title="ERRO", message='Digite todos os dados antes de Salvar')
         return
     Tabela.insert("", "end", values=(ID_text['text'], Data_Monitoria_Text.get(), Monitorado_Por_Text.get(), Nome_Analisado_Text.get(), Matricula_Text.get(), Celula_Text.get(),
-                                     Fila_Text.get(), Protocolo_Text.get(), Motivo_do_contato_Text.get(), Data_e_hora_atendimento_Text.get()))
+                                     Fila_Text.get(), Protocolo_Text.get(), Motivo_do_contato_Text.get(), Data_e_hora_atendimento_Text.get(), NotaFinal_Label['text']))
 
 
 # Função para salvar os dados salvos nas caixas de texto  ----------------------------------------
@@ -55,13 +55,18 @@ def dados():
 
 
 def item1_def():
-    valoritem1 = item1_value[1]
+    valoritem1 = item_value10[1]
     item1_label['text'] = valoritem1
 
 
 def item2_def():
-    valoritem2 = item2_value[1]
+    valoritem2 = item_value15[1]
     item2_label['text'] = valoritem2
+
+
+def item3_def_100():
+    valoritem3100 = item_value100[1]
+    item9_label['text'] = valoritem3100
 
 
 # Informações sobre tamanho da Janela, cor de fundo, icone da Janela e Título da Janela --
@@ -69,7 +74,6 @@ Janela = Tk()
 Janela.title('       Relatório de Monitoria')
 Janela.geometry('1300x2000')
 Janela.configure(bg='white')
-Janela.iconphoto(False, PhotoImage(file=fr'C:\Users\dougl\Downloads\via-logo-2-599x433.png'))
 
 
 # Canvas
@@ -118,9 +122,9 @@ Monitorado_Por_Text.place(x=210, y=64)
 
 Data_Monitoria_Text = Entry(frame, width=20, border="2")
 Data_Monitoria_Text.insert(0, Today)
-Data_Monitoria_Text.place(x=500, y=56)
+Data_Monitoria_Text.place(x=500, y=64)
 Data_Monitoria = Label(frame, width=25, height=1, justify="center", text="Data da Monitoria", bg=Cinza_Claro, anchor=W)
-Data_Monitoria.place(x=638, y=56)
+Data_Monitoria.place(x=638, y=64)
 
 Nome_Analisado = Label(frame, width=25, height=1, justify="center", text="Nome Analisado:", bg=Cinza_Claro, anchor=W)
 Nome_Analisado.place(x=20, y=105)
@@ -169,19 +173,43 @@ Desconto.place(x=621, y=450)
 NotaFinal = Label(frame, width=10, height=2, text="Nota Final", bg=Cinza, font='Arial 10')
 NotaFinal.place(x=775, y=450)
 
+NotaFinal_Label = Label(frame, width=10, height=1, )
+NotaFinal_Label.place(x=785, y=480)
 
 # Itens para o checklist ---------------------------------------------------------------
-item1 = Checkbutton(frame, width=80, height=1, text='Entrou em contato com o cliente', offvalue=0, command=item1_def,
+item1 = Checkbutton(frame, width=80, height=1, text='Tratativa ocorreu no prazo de 24h', offvalue=0, command=item1_def,
                     anchor=W)
 item1.place(x=10, y=480,)
-item2 = Checkbutton(frame, width=80, height=1, text='Analisou o pedido', command=item2_def, anchor=W)
+item2 = Checkbutton(frame, width=80, height=1, text='2.1 Avaliou Ponto de Controle primeira entrega',
+                    command=item2_def, anchor=W)
 item2.place(x=10, y=500)
-item3 = Checkbutton(frame, width=80, height=1, text='Fez o processo corretamente', anchor=W)
+item3 = Checkbutton(frame, width=80, height=1, text='2.2 Avaliou Ponto de Controle da intância de Reversa/troca/cancelamento',
+                    anchor=W, command=item2_def)
 item3.place(x=10, y=520)
-item4 = Checkbutton(frame, width=80, height=1, text='Nao colocou justificativa', anchor=W)
+item4 = Checkbutton(frame, width=80, height=1, text='2.3 Avaliou histórico de Atendimento e interação CRL',
+                    anchor=W, command=item2_def)
 item4.place(x=10, y=540)
-item5 = Checkbutton(frame, width=80, height=1, text='Nao finalizou o protocolo', anchor=W)
+item5 = Checkbutton(frame, width=80, height=1, text='2.4 Avaliou descrição do Solicitante',
+                    anchor=W, command=item1_def)
 item5.place(x=10, y=560)
+item6 = Checkbutton(frame, width=80, height=1, text='2.5 Avaliou aba de anexos com os comprovantes/Autorização',
+                    anchor=W, command=item2_def)
+item6.place(x=10, y=580)
+item7 = Checkbutton(frame, width=80, height=1, text='3.1 Aplicou solução correta (IF aprovada/IF reprovada/Encerrar tratativa)',
+                    anchor=W, command=item1_def)
+item7.place(x=10, y=600)
+item8 = Checkbutton(frame, width=80, height=1, text='3.2 Inseriu justificativa',
+                    anchor=W, command=item1_def)
+item8.place(x=10, y=620)
+item9 = Checkbutton(frame, width=80, height=1, text='Não liberação da instancia com pontos de controle elegíveis ',
+                    anchor=W, command=item3_def_100)
+item9.place(x=10, y=640)
+item10 = Checkbutton(frame, width=80, height=1, text='Liberação da Instancia de forma indevida - sem ponto finalizador de acordo com política',
+                    anchor=W, command=item3_def_100)
+item10.place(x=10, y=660)
+item11 = Checkbutton(frame, width=80, height=1, text='Direcionamento indevido paras as áreas',
+                    anchor=W, command=item3_def_100)
+item11.place(x=10, y=680)
 
 
 # Valor do item do Checklist
@@ -189,44 +217,66 @@ item1_label = Label(frame, width=10, height=1)
 item1_label.place(x=620, y=480)
 item2_label = Label(frame, width=10, height=1)
 item2_label.place(x=620, y=500)
+item3_label = Label(frame, width=10, height=1)
+item3_label.place(x=620, y=520)
+item4_label = Label(frame, width=10, height=1)
+item4_label.place(x=620, y=540)
+item5_label = Label(frame, width=10, height=1)
+item5_label.place(x=620, y=560)
+item6_label = Label(frame, width=10, height=1)
+item6_label.place(x=620, y=580)
+item7_label = Label(frame, width=10, height=1)
+item7_label.place(x=620, y=600)
+item8_label = Label(frame, width=10, height=1)
+item8_label.place(x=620, y=620)
+item9_label = Label(frame, width=10, height=1)
+item9_label.place(x=620, y=640)
+item10_label = Label(frame, width=10, height=1)
+item10_label.place(x=620, y=660)
+item11_label = Label(frame, width=10, height=1)
+item11_label.place(x=620, y=680)
 
-NotaFinal_Label = Label(frame, width=10, height=1)
-NotaFinal_Label.place(x=780, y=480)
 
+Tabela_titulo = Label(frame, width=10, height=1, text="Tabela", bg=Cinza, font='Arial 11')
+Tabela_titulo.place(x=50, y=773)
 
 # Tabela de dados na frame Tkinter -----------------------------------------------------
-Tabela = ttk.Treeview(frame, columns=("column1", "column2", "column3", "column4", "column5", "column6",
-                                                             "column7", "column8", "column9", "column10"), show='headings')
-Tabela.column('column1', width=100, minwidth=50, stretch=NO)
+Tabela = ttk.Treeview(frame, selectmode=EXTENDED, columns=("column1", "column2", "column3", "column4", "column5", "column6",
+                                                             "column7", "column8", "column9", "column10", "column11"),
+                      show='headings')
+Tabela.column('column1', width=50, minwidth=50, stretch=NO)
 Tabela.heading('#1', text='ID')
-Tabela.place(x=10, y=700)
+Tabela.place(x=10, y=800)
 Tabela.column('column2', width=100, minwidth=50, stretch=NO)
 Tabela.heading('#2', text='Data da Monitoria')
-Tabela.place(x=10, y=700)
+Tabela.place(x=10, y=800)
 Tabela.column('column3', width=100, minwidth=50, stretch=NO)
 Tabela.heading('#3', text='Monitorado Por')
-Tabela.place(x=10, y=700)
-Tabela.column('column4', width=150, minwidth=50, stretch=NO)
+Tabela.place(x=10, y=800)
+Tabela.column('column4', width=120, minwidth=50, stretch=NO)
 Tabela.heading('#4', text='Nome Analisado')
-Tabela.place(x=10, y=700)
+Tabela.place(x=10, y=800)
 Tabela.column('column5', width=100, minwidth=50, stretch=NO)
 Tabela.heading('#5', text='Matricula')
-Tabela.place(x=10, y=700)
-Tabela.column('column6', width=100, minwidth=50, stretch=NO)
+Tabela.place(x=10, y=800)
+Tabela.column('column6', width=80, minwidth=50, stretch=NO)
 Tabela.heading('#6', text='Célula')
-Tabela.place(x=10, y=700)
-Tabela.column('column7', width=100, minwidth=50, stretch=NO)
+Tabela.place(x=10, y=800)
+Tabela.column('column7', width=80, minwidth=50, stretch=NO)
 Tabela.heading('#7', text='Fila')
-Tabela.place(x=10, y=700)
+Tabela.place(x=10, y=800)
 Tabela.column('column8', width=100, minwidth=50, stretch=NO)
 Tabela.heading('#8', text='Protocolo')
-Tabela.place(x=10, y=700)
+Tabela.place(x=10, y=800)
 Tabela.column('column9', width=100, minwidth=50, stretch=NO)
 Tabela.heading('#9', text='Motivo do Contato')
-Tabela.place(x=10, y=700)
-Tabela.column('column10', width=200, minwidth=50, stretch=NO)
+Tabela.place(x=10, y=800)
+Tabela.column('column10', width=100, minwidth=50, stretch=NO)
 Tabela.heading('#10', text='Data e hora do atendimento')
-Tabela.place(x=10, y=700)
+Tabela.place(x=10, y=800)
+Tabela.column('column11', width=50, minwidth=50, stretch=NO)
+Tabela.heading('#11', text='Nota Final')
+Tabela.place(x=10, y=800)
 
 Todos_os_direitos_reservados = Label(frame, width=50, bg=Cinza, text=All_rights_reserved)
 Todos_os_direitos_reservados.place(x=810, y=1900)
